5d08b78d92bc2f1df0f2fb22d3149636cb5b89a9
Saurav Raj <er.sauravraj12@gmail.com>
2025-07-03 17:18:02 +0530

    day-1 complete

--- server.js
+++ server.js
@@ -7,10 +7,29 @@
 const express = require('express');  // is prakar se express ka instance bana liye
 const app = express();  // express ka single 
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json()); 
+
 app.listen(3000,() => {
     console.log("Server started at port no 3000");
 });
 
-app.get('/',(req,res) => {
-    res.send("Hello Jee, Kaise ho sare")
+app.get('/',(request,response) => {
+    response.send("Hello Jee, Kaise ho sare")
+})
+
+app.post('/api/cars', (request,response) => {
+    const {name, brand} = request.body;
+    console.log(name);
+    console.log(brand);
+    response.send(`Cars details submitted Successfully! `)
+})
+
+//Connecting server through DB using Mongoose
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/myDatabase')
+.then(console.log('Connected to DB successful!'))
+.catch((error) => {
+    console.log('Connection to DB Failed!')
 })
